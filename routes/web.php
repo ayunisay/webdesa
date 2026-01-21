db965db7f3430885d6b4a1dd1fc9181a5826dae7
ayunisa <ayunisa1412@gmail.com>
2026-01-21 22:41:21 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,14 @@ use App\Http\Controllers\PosyanduController;
 use App\Http\Controllers\RtRwController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\KehamilanController;
 
-Route::get('/', function () {
-    // Arahkan langsung ke halaman login jika belum login
-    return redirect()->route('login');
-});
-
-Route::controller(AuthController::class)->group(function () {
-    Route::get('/login', 'showLoginForm')->name('login');
-    Route::post('/login', 'login')->name('login.submit');
-    Route::get('/logout', 'logout')->name('logout');
-});
-
-//user view
-Route::middleware(['auth'])->group(function() {
-    Route::middleware(['role:user'])->group(function() {
         Route::get('/user-dashboard', [DashboardController::class, 'dashboardUser'])->name('dashboardUser');
 
         Route::get('/posyandu', [PosyanduController::class, 'index'])->name('posyandu');
 
         // //kehamilan
         Route::get('/posyandu-kehamilan', [PosyanduController::class, 'kehamilanview'])->name('posyandu-kehamilan');
-        //tambah kehamilan
-        Route::post('/tambah-data-kehamilan', [KehamilanController::class, 'tambahDataKehamilan'])->name('tambah-data-kehamilan');
-
+       
         //data anak
         Route::get('/posyandu-data-anak', [PosyanduController::class, 'dataAanakView'])->name('posyandu-data-anak');
         //data lansia
@@ -47,12 +30,4 @@ Route::middleware(['auth'])->group(function() {
         Route::get('/pelaporan-kepindahan-datang', [RtRwController::class, 'laporanKepindahanKedatangan'])->name('pelaporan-kepindahan-datang');
         //laporan tindak kriminal
         Route::get('/pelaporan-tindak-kriminal', [RtRwController::class, 'laporanTindakKriminal'])->name('pelaporan-tindak-kriminal');
-    });
-
-
-//admin view
-    Route::middleware(['role:admin'])->group(function() {
-        Route::get('/admin-dashboard', [DashboardController::class, 'dashboardAdmin'])->name('dashboardAdmin');
-    });
-});
 
